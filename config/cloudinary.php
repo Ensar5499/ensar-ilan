1581142d75f02fc245f6f7d7dea2ea586a304858
Ensar5499 <ensarkulaksiz54@gmail.com>
2026-05-11 22:10:45 +0300

    Cloudinary config fallback eklendi

--- config/cloudinary.php
+++ config/cloudinary.php
@@ -1,5 +1,21 @@
 <?php
 
+/*
+ * This file is part of the Laravel Cloudinary package.
+ *
+ * (c) Cloudinary Labs
+ */
+
 return [
+    /*
+    |--------------------------------------------------------------------------
+    | Cloudinary Configuration
+    |--------------------------------------------------------------------------
+    |
+    | Here you may configure your Cloudinary settings. Cloudinary is a cloud
+    | service that offers a solution to a web application's entire image
+    | management pipeline.
+    |
+    */
     'cloudinary_url' => env('CLOUDINARY_URL', 'cloudinary://598232723132484:bLim7bUknk5Y0ppMLmzCFwwFp6Y@dzoowxtjc'),
 ];
