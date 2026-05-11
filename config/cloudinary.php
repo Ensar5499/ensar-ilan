83d0fb61025de6a7a455b7614201300e22ba8843
Ensar5499 <ensarkulaksiz54@gmail.com>
2026-05-11 22:04:37 +0300

    Config fallback added to prevent provider crash

--- config/cloudinary.php
+++ config/cloudinary.php
@@ -1,4 +1,5 @@
 <?php
+
 return [
-    'cloudinary_url' => env('CLOUDINARY_URL'),
+    'cloudinary_url' => env('CLOUDINARY_URL', 'cloudinary://598232723132484:bLim7bUknk5Y0ppMLmzCFwwFp6Y@dzoowxtjc'),
 ];
